4ee571e4e605115f96ddb3e2093d7d01de06012c
Melissendra <melissendra77@gmail.com>
2021-04-20 17:02:47 +0200

    links wiht back repaired

--- src/features/users/components/account/Account.js
+++ src/features/users/components/account/Account.js
@@ -5,7 +5,6 @@ import "./Account.scss";
 export default function Account( userInfo ){
 	const displayInfo = ( userInfo ) =>{
 		const info = userInfo.userInfo
-		console.log( info)
 		return(
 			<div className="account_container" key={info._id}>
 				<div className="username">
